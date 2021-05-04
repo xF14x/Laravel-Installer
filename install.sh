02488c70cfb23d4e843a471397bee2fd3ee3ce3c
xF14x <>
2021-05-04 07:26:15 +0300

    Programme The Script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Update The Packages.."
+sudo apt update && sudo apt full-upgrade -y
+wait
+echo "Done!"
+echo "Install The Packages.."
+sleep 1
+sudo apt install apache2 php libapache2-mod-php php-mbstring php-xml php-tokenizer php-zip php-curl php-json mysql-server phpmyadmin composer -y
+wait
+read -p "The Dir to install: " Dir
+wait
+cd $Dir && composer create-project --prefer-dist laravel/laravel site -vvv
+wait
+sudo rm -rf /var/www/html
+wait
+sudo ln -s $Dir/site/public /var/www/html
+cd site/ && chmod 777 storage/ -R && chmod 777 bootstrap/cache -R
+clear
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+echo "now enable the apache2 rewrite mod \"AllowIverride\""
+echo "and thene enter \"sudo a2enmod rewrite\""
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+echo "and enter \"sudo mysql_secure_installation\""
+echo "CREATE USER 'YOUR-USERNAME'@'localhost' IDENTIFIED BY 'YOUR-PASSWORD';"
+echo "GRANT ALL PRIVILEGES ON *.* TO 'YOUR-USERNAME'@'localhost' WITH GRANT OPTION;"
+echo "FLUSH PRIVILEGES;"
+
+exec bash
